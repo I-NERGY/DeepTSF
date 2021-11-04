c46eac0ee2c950379c24696e3937ea5dda7fb96e
sotiris pelekis <spelekis@epu.ntua.gr>
2021-11-04 16:55:15 +0200

    - Renamed files
    Former-commit-id: 59059dbfb460b857d420d5265438f64077f0147a

--- PILOT2_VEOLIA/tal_prophet_fit_and_store.py
+++ PILOT2_VEOLIA/tal_prophet_fit_and_store.py
@@ -7,22 +7,22 @@ import pickle
 from datetime import timedelta
 
 # Load dataset
-ts60 = pd.read_csv('../VEOLIA/artifacts/timeseries_60min.csv', index_col=0, parse_dates=True)
+ts60 = pd.read_csv('../../VEOLIA/artifacts/timeseries_60min.csv', index_col=0, parse_dates=True)
 load60 = ts60['Diff Load Activa Total (60 minuto)'].dropna()
 load60.head()
 
 # Load calendar
-cal60 = pd.read_csv('../VEOLIA/artifacts/time_60min.csv', index_col=0, parse_dates=True)
+cal60 = pd.read_csv('../../VEOLIA/artifacts/time_60min.csv', index_col=0, parse_dates=True)
 
 holidays = pd.DataFrame({
   'holiday': 'Valladolid',
-  'ds': np.unique(pd.to_datetime(cal60[cal60['holiday']==1]['datetime'], 
+  'ds': np.unique(pd.to_datetime(cal60[cal60['holiday']==1]['datetime'],
         format="%Y-%m-%d %H:%M:%S").apply(lambda x:x.date()).values).tolist(),
 })
 
 ## Train / test split
 last_train_day = datetime(2021, 6, 20)
-train = load60[:(last_train_day - timedelta(hours=1))]  
+train = load60[:(last_train_day - timedelta(hours=1))]
 train = train.asfreq('H')
 test = load60[last_train_day:]
 test = test.asfreq('H')
@@ -41,12 +41,12 @@ model_prophet = Prophet(holidays = holidays)
 model_prophet.fit(train_prophet)
 
 ## Save model locally
-fname = "../VEOLIA/models/tal_prophet.pkl"  
-with open(fname, 'wb') as file:  
+fname = "../../VEOLIA/models/tal_prophet.pkl"  
+with open(fname, 'wb') as file:
     pickle.dump(model_prophet, file)
 
 # quick evaluation
-from sklearn.metrics import mean_squared_error as mse 
+from sklearn.metrics import mean_squared_error as mse
 from sklearn.metrics import mean_absolute_percentage_error as mape
 steps=24
 future = pd.DataFrame(test.index[:steps]).rename(columns={'datetime':'ds'})
