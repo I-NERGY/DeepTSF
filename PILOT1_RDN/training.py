ac3b3a5ff4c8caa12e1dd3d13ddfa979defae279
sotiris pelekis <spelekis@epu.ntua.gr>
2022-05-06 14:19:45 +0300

    - Updated training.py to produce pyfunc models

--- PILOT1_RDN/training.py
+++ PILOT1_RDN/training.py
@@ -1,14 +1,16 @@
 import pretty_errors
 from utils import none_checker, ConfigParser, download_online_file, load_local_csv_as_darts_timeseries, truth_checker #, log_curves
 from preprocessing import scale_covariates, split_dataset
+# from inference import _MLflowDartsModelWrapper, mlflow_serve_conda_env
 
 # the following are used through eval(darts_model + 'Model')
 from darts.models import RNNModel, BlockRNNModel, NBEATSModel, TFTModel, NaiveDrift, NaiveSeasonal, TCNModel
-from darts.models.forecasting.auto_arima import AutoARIMA
+# from darts.models.forecasting.auto_arima import AutoARIMA
 from darts.models.forecasting.gradient_boosted_model import LightGBMModel
 from darts.models.forecasting.random_forest import RandomForest
 from darts.utils.likelihood_models import ContinuousBernoulliLikelihood, GaussianLikelihood, DirichletLikelihood, ExponentialLikelihood, GammaLikelihood, GeometricLikelihood
 
+import yaml
 import mlflow
 import click
 import os
@@ -17,6 +19,7 @@ import logging
 import pickle
 import tempfile
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+import shutil
 
 # get environment variables
 from dotenv import load_dotenv
@@ -143,6 +146,7 @@ def train(series_csv, series_uri, future_covs_csv, future_covs_uri,
 
     # redirect to local location of downloaded remote file
     if series_uri is not None:
+        print(series_uri)
         download_file_path = download_online_file(series_uri, dst_filename="load.csv")
         series_csv = download_file_path
     if  future_covs_uri is not None:
@@ -165,6 +169,7 @@ def train(series_csv, series_uri, future_covs_csv, future_covs_uri,
         """
         past_covs_csv = future_covs_csv
         future_covs_csv = None
+        # TODO: when actual weather comes extend it, now the stage only accepts future covariates as argument.
 
     elif darts_model in ["RNN"]:
         """Does not accept past covariates as it needs to know future ones to provide chain forecasts
@@ -173,36 +178,41 @@ def train(series_csv, series_uri, future_covs_csv, future_covs_uri,
         feature space will change during inference. If for example I have current temperature and during 
         the forecast chain I only have time covariates, as I won't know the real temp then a constant \
         architecture like LSTM cannot handle this"""
-        future_covs_csv = past_covs_csv
         past_covs_csv = None
+        # TODO: when actual weather comes extend it, now the stage only accepts future covariates as argument.
     #elif: extend for other models!! (time_covariates are always future covariates, but some models can't handle them as so)
 
     future_covariates = none_checker(future_covs_csv)
     past_covariates = none_checker(past_covs_csv)
 
     with mlflow.start_run(run_name=f'train_{darts_model}', nested=True) as mlrun:
+
+        mlflow_model_root_dir = "pyfunc_model"
+        
         ######################
         # Load series and covariates datasets
+        time_col = "Date"
         series = load_local_csv_as_darts_timeseries(
                 local_path=series_csv, 
                 name='series', 
-                time_col='Date', 
+                time_col=time_col, 
                 last_date=test_end_date)
         if future_covariates is not None:
             future_covariates = load_local_csv_as_darts_timeseries(
                 local_path=future_covs_csv, 
                 name='future covariates', 
-                time_col='Date', 
+                time_col=time_col,
                 last_date=test_end_date)
         if past_covariates is not None:
             past_covariates = load_local_csv_as_darts_timeseries(
                 local_path=past_covs_csv, 
                 name='past covariates', 
-                time_col='Date', 
+                time_col=time_col,
                 last_date=test_end_date)
 
         print("\nCreating local folder to store the scaler as pkl...")
         logging.info("\nCreating local folder to store the scaler as pkl...")
+        
         if scale:
             scalers_dir = tempfile.mkdtemp()
         features_dir = tempfile.mkdtemp()
@@ -246,21 +256,21 @@ def train(series_csv, series_uri, future_covs_csv, future_covs_uri,
         logging.info("\nScaling...")
 
         ## scale series
-        scaled_series = scale_covariates(
+        series_transformed = scale_covariates(
             series_split, 
             store_dir=features_dir, 
             filename_suffix="series_transformed.csv", 
             scale=scale)
         if scale:
-            pickle.dump(scaled_series["transformer"], open(f"{scalers_dir}/scaler_series.pkl", "wb"))
+            pickle.dump(series_transformed["transformer"], open(f"{scalers_dir}/scaler_series.pkl", "wb"))
         ## scale future covariates
-        scaled_future_covariates = scale_covariates(
+        future_covariates_transformed = scale_covariates(
             future_covariates_split, 
             store_dir=features_dir, 
             filename_suffix="future_covariates_transformed.csv",
             scale=scale_covs)
         ## scale past covariates
-        scaled_past_covariates = scale_covariates(
+        past_covariates_transformed = scale_covariates(
             past_covariates_split,
             store_dir=features_dir, 
             filename_suffix="past_covariates_transformed.csv",
@@ -295,23 +305,17 @@ def train(series_csv, series_uri, future_covs_csv, future_covs_uri,
             )                
             ## fit model
             # try:
-            # print(scaled_series['train'])
-            # print(scaled_series['val'])
-            model.fit(scaled_series['train'],
-                future_covariates=scaled_future_covariates['train'],
-                past_covariates=scaled_past_covariates['train'],
-                val_series=scaled_series['val'],
-                val_future_covariates=scaled_future_covariates['val'],
-                val_past_covariates=scaled_past_covariates['val'])
-
-            # TODO: Package Models as python functions for MLflow (see RiskML and https://mlflow.org/docs/0.5.0/models.html#python-function-python-function)
-            print('\nStoring torch model to MLflow...')
-            logging.info('\nStoring torch model to MLflow...')
+            # print(series_transformed['train'])
+            # print(series_transformed['val'])
+            model.fit(series_transformed['train'],
+                future_covariates=future_covariates_transformed['train'],
+                past_covariates=past_covariates_transformed['train'],
+                val_series=series_transformed['val'],
+                val_future_covariates=future_covariates_transformed['val'],
+                val_past_covariates=past_covariates_transformed['val'])
             
             logs_path = f"./darts_logs/{mlrun.info.run_id}/"
-
-            mlflow.log_artifacts(logs_path, "model")
-            
+            model_type = "pl"
             # TODO: Implement this step without tensorboard (fix utils.py: get_training_progress_by_tag)
             # log_curves(tensorboard_event_folder=f"./darts_logs/{mlrun.info.run_id}/logs", output_dir='training_curves')
 
@@ -341,12 +345,12 @@ def train(series_csv, series_uri, future_covs_csv, future_covs_uri,
             logging.info(f'\nTraining {darts_model}...')
 
             model.fit(
-                series=scaled_series['train'],
-                # val_series=scaled_series['val'],
-                future_covariates=scaled_future_covariates['train'],
-                past_covariates=scaled_past_covariates['train'], 
-                # val_future_covariates=scaled_future_covariates['val'],
-                # val_past_covariates=scaled_past_covariates['val']
+                series=series_transformed['train'],
+                # val_series=series_transformed['val'],
+                future_covariates=future_covariates_transformed['train'],
+                past_covariates=past_covariates_transformed['train'], 
+                # val_future_covariates=future_covariates_transformed['val'],
+                # val_past_covariates=past_covariates_transformed['val']
                 )
 
             print('\nStoring the model as pkl to MLflow...')
@@ -354,74 +358,103 @@ def train(series_csv, series_uri, future_covs_csv, future_covs_uri,
             forest_dir = tempfile.mkdtemp()
 
             pickle.dump(model, open(
-                f"{forest_dir}/model_best_{darts_model}.pkl", "wb"))
+                f"{forest_dir}/_model_{darts_model}.pkl", "wb"))
+            
+            logs_path = forest_dir 
+            model_type = "pkl"
 
-            mlflow.log_artifacts(forest_dir, "model")
-        
-        # ARIMA
-        elif darts_model == 'AutoARIMA':
-            hparams_to_log={}
-            print("\nEmploying AutoARIMA - Ignoring hyperparameters")
-            logging.info("\nEmploying AutoARIMA - Ignoring hyperparameters")
-            model = AutoARIMA(random_state=0, information_criterion='bic')
-            model.fit(scaled_series['train'].append(scaled_series['val']), future_covariates=future_covariates)
-            # hparams = model.get_params()
-            # print(model.summary())
-
-            print("\nStoring the model as pkl...")
-            logging.info("\nStoring the model as pkl...")
-            arima_dir = tempfile.mkdtemp()
-
-            pickle.dump(model, open(
-                f"{arima_dir}/model_best_arima.pkl", "wb"))
-            mlflow.log_artifacts(arima_dir)
-
-
+        ######################
         # Log hyperparameters
         mlflow.log_params(hparams_to_log)
+
+        ######################
+        # Log artifacts
+        ## Move scaler in logs path
+        if scale:
+            source_dir = scalers_dir
+            target_dir = logs_path
+            file_names = os.listdir(source_dir)
+            for file_name in file_names:
+                shutil.move(os.path.join(source_dir, file_name), 
+                target_dir)
+        
+        ## Create and Move model info in logs path
+        model_info_dict = {
+            "darts_forecasting_model":  model.__class__.__name__,
+            "run_id": mlrun.info.run_id
+            }
+        with open('model_info.yml', mode='w') as outfile:
+            yaml.dump(
+                model_info_dict,
+                outfile,
+                default_flow_style=False)
+        shutil.move('model_info.yml', target_dir)
+
+        ## Rename logs path to get rid of run name
+        logs_path_new = logs_path.replace(
+            mlrun.info.run_id, "model_artifacts")
+        os.rename(logs_path, logs_path_new)
+
+        ## Log MLflow model
+        mlflow.pyfunc.log_model(mlflow_model_root_dir,
+                                loader_module="inference",
+                                data_path=logs_path_new)
+
+        ## Clean logs_path: Now it is necessary to avoid conflicts
+        shutil.rmtree(logs_path_new)
+
+        print("\nArtifacts uploaded.")
+        logging.info("\nArtifacts uploaded.")
         
         ######################
-        # Log artifacts and set respective tags
-        print("\nDatasets and scalers are being uploaded to MLflow...")
-        logging.info("\nDatasets and scalers are being uploaded to MLflow...")
+        # Set tags
+        print("\nArtifacts are being uploaded to MLflow...")
+        logging.info("\nArtifacts are being uploaded to MLflow...")
         mlflow.log_artifacts(features_dir, "features")
 
         if scale:
-            mlflow.log_artifacts(scalers_dir, "scalers")
+            # mlflow.log_artifacts(scalers_dir, f"{mlflow_model_path}/scalers")
             mlflow.set_tag(
-                'scaler_uri', f'{mlrun.info.artifact_uri}/scalers/scaler_series.pkl')
+                'scaler_uri', 
+                f'{mlrun.info.artifact_uri}/{mlflow_model_root_dir}/data/model_artifacts/scaler_series.pkl')
         else:
             mlflow.set_tag('scaler_uri', 'mlflow_artifact_uri')
 
         mlflow.set_tag("run_id", mlrun.info.run_id)
         mlflow.set_tag("stage", "training")
+        mlflow.set_tag("model_type", model_type)
 
-        client = mlflow.tracking.MlflowClient()
-        artifact_list = client.list_artifacts(run_id=mlrun.info.run_id, path='model')
+        mlflow.set_tag("darts_forecasting_model", 
+            model.__class__.__name__)
 
-        mlflow_path = [
-            fileinfo.path for fileinfo in artifact_list if 'pth.tar' in fileinfo.path or '.pkl' in fileinfo.path][0]
-        mlflow.set_tag('model_uri', mlflow.get_artifact_uri(mlflow_path))
-
-        mlflow.set_tag("darts_forecasting_model", model.__class__.__name__)
-
-        mlflow.set_tag('series_uri', f'{mlrun.info.artifact_uri}/features/series.csv')
+        mlflow.set_tag('series_uri', 
+            f'{mlrun.info.artifact_uri}/features/series.csv')
 
         if future_covariates is not None:
-            mlflow.set_tag('future_covariates_uri', f'{mlrun.info.artifact_uri}/features/future_covariates_transformed.csv')
+            mlflow.set_tag(
+                'future_covariates_uri', 
+                f'{mlrun.info.artifact_uri}/features/future_covariates_transformed.csv')
         else:
-            mlflow.set_tag('future_covariates_uri', 'mlflow_artifact_uri')
+            mlflow.set_tag(
+                'future_covariates_uri', 
+                'mlflow_artifact_uri')
 
         if past_covariates is not None:
-            mlflow.set_tag('past_covariates_uri', f'{mlrun.info.artifact_uri}/features/past_covariates_transformed.csv')
+            mlflow.set_tag(
+                'past_covariates_uri', 
+                f'{mlrun.info.artifact_uri}/features/past_covariates_transformed.csv')
         else:
-            mlflow.set_tag('past_covariates_uri', 'mlflow_artifact_uri')
+            mlflow.set_tag('past_covariates_uri', 
+                'mlflow_artifact_uri')
 
-        mlflow.set_tag('setup_uri', f'{mlrun.info.artifact_uri}/features/split_info.yml')
-
-        print("\nArtifacts uploaded.")
-        logging.info("\nArtifacts uploaded.")
+        mlflow.set_tag(
+            'setup_uri', 
+            f'{mlrun.info.artifact_uri}/features/split_info.yml')
         
+        # model_uri
+        mlflow.set_tag('model_uri', mlflow.get_artifact_uri(
+            f"{mlflow_model_root_dir}/data/model_artifacts"))
+
         return
 
 if __name__ =='__main__':
