f7345721dd5a7bf04d6407da1f53ec02402b9721
sotiris pelekis <spelekis@epu.ntua.gr>
2022-05-17 12:27:11 +0300

    - Minor changes

--- PILOT1_RDN/utils.py
+++ PILOT1_RDN/utils.py
@@ -87,6 +87,7 @@ def load_local_pl_model(model_root_dir):
     model = eval(darts_forecasting_model)
     
     # model_root_dir = model_root_dir.replace('/', os.path.sep)
+    
     print(f"Loading model from local directory:{model_root_dir}")
     
     best_model = model.load_from_checkpoint(model_root_dir, best=True)
