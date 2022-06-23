d7b20e8b4ec9cb491b2feaf1c660f3d5a45c86ae
sotiris pelekis <spelekis@epu.ntua.gr>
2022-06-23 15:41:28 +0300

    - Added dataset limits in csv validation in api.py

--- PILOT1_RDN/api.py
+++ PILOT1_RDN/api.py
@@ -8,6 +8,7 @@ import tempfile
 import os
 from load_raw_data import read_and_validate_input
 from exceptions import DatesNotInOrder, WrongColumnNames
+from datetime import datetime
 
 # allows automated type check with pydantic
 class ModelName(str, Enum):
@@ -54,31 +55,6 @@ async def root():
 async def get_model_names():
     return ModelName.dict()
 
-# @app.get("/models/{model_name}/hparams")
-# async def get_model_hparams():
-#     return ModelName.dict()
-
-
-# Validate
-@app.post('upload/validateCSVfile/')
-async def validate_csv_file(fname: str, day_first: bool):
-    params = {
-        "series_csv": fname,  # get from create_upload_csv_file()
-        "day_first": day_first # get from ui (tickbox or radio button or smth...), allowed values: true or false, default: false
-    }
-
-    fileExtension = fname.split(".")[-1].lower() == "csv"
-    if not fileExtension:
-        raise HTTPException(
-            status_code=415, detail="Unsupported file type provided. Please upload CSV file)
-    try:
-        read_and_validate_input(fname, day_first)
-    except DatesNotInOrder:
-        return {"message": "There was an error validating the file. Dates are not in order",
-                "fname": fname}
-    except WrongColumnNames:
-        return {"message": "There was an error validating the file. Please reupload CSV with 2 columns with names: 'Date', 'Load'"}
-
 @app.post('/upload/uploadCSVfile/')
 async def create_upload_csv_file(file: UploadFile = File(...)):
     try:
@@ -92,7 +68,7 @@ async def create_upload_csv_file(file: UploadFile = File(...)):
         return {"message": "There was an error uploading the file"}
     finally:
         await file.close()
-    return {"message": "There was an error uploading the file",
+    return {"message": "Upload successful",
             "fname": fname}
 
     # if load_raw_data_run.info.status == "FAILED":
@@ -109,6 +85,33 @@ async def create_upload_csv_file(file: UploadFile = File(...)):
     #             "dataset_end": load_raw_data_run.data.tags['dataset_end']
     #             }
 
+# Validate right after upload
+@app.post('/upload/validateCSVfile/')
+async def validate_csv_file(fname: str, day_first: bool):
+    #TODO: korbakis while uploading ask the user if day_first or month first (default: dayfirst=False)
+    params = {
+        "series_csv": fname,  # get from create_upload_csv_file()
+        # get from ui (tickbox or radio button or smth...), allowed values: true or false, default: false
+        "day_first": day_first
+    }
+
+    fileExtension = fname.split(".")[-1].lower() == "csv"
+    if not fileExtension:
+        raise HTTPException(
+            status_code=415, detail="Unsupported file type provided. Please upload CSV file")
+    try:
+        ts = read_and_validate_input(fname, day_first)
+    except DatesNotInOrder:
+        return {"message": "There was an error validating the file. Dates are not in order",
+                "fname": fname}
+    except WrongColumnNames:
+        return {"message": "There was an error validating the file. Please reupload CSV with 2 columns with names: 'Date', 'Load'"}
+    return {"message": "Validation successful",
+            "fname": fname,
+            "dataset_start": datetime.strftime(ts.index[0], "%Y%m%d"),
+            "dataset_end": datetime.strftime(ts.index[0], "%Y%m%d")
+            }
+
 @app.get('/experimentation_pipeline/training/hyperparameter_entrypoints/')
 async def get_experimentation_pipeline_hparam_entrypoints():
     return ConfigParser().read_entrypoints()
@@ -119,17 +122,17 @@ async def get_resolutions():
 
 @app.post('/experimentation_pipeline/run_all')
 async def run_experimentation_pipeline(parameters: dict):
-
+    #TODO: korbakis how can you obtain these data?
     params = {
-        # get it from create_upload_file
-        "series_uri": parameters["series_uri"], # input: get value from from @app.post('/uploadfile/') | type: str | example: 
-        "experiment_name": parameters["experiment_name"], # input: get it from @app.post('/uploadfile/') | type: str | example: 
+        "series_uri": parameters["series_uri"], # input: get value from @app.post('/upload/validateCSVfile/') | type: str | example: -
+        "experiment_name": parameters["experiment_name"], # input: user | type: str | example: ml_experiment
         "resolution": parameters["resolution"], # input: user | type: str | example: "15" | get allowed values from @app.get('/experimentation_pipeline/etl/get_resolutions/')
-        "cut_date_val": parameters["validation_start_date"], # input: user | type: str | example: 20201101 | choose from calendar, should be > dataset_start and < dataset_end
+        "cut_date_val": parameters["validation_start_date"], # input: user | type: str | example: "20201101" | choose from calendar, should be > dataset_start and < dataset_end
         "cut_date_test": parameters["test_start_date"], # input: user | type: str | example: "20210101" | Choose from calendar, should be > cut_date_val and < dataset_end
-        "test_end_date": parameters["test_end_date"],  # input: user | type: str | example: "20220101" | Choose from calendar, should be > cut_date_test, defaults to dataset_end
+        "test_end_date": parameters["test_end_date"],  # input: user | type: str | example: "20220101" | Choose from calendar, should be > cut_date_test and <= dataset_end, defaults to dataset_end
         "darts_model": parameters["model"], # input: user | type: str | example: "nbeats" | get values from @app.get("/models/get_model_names")
-        "forecast_horizon": parameters["forecast_horizon"], # input: user | type: str | example: "96" | should be int > 0
+        "forecast_horizon": parameters["forecast_horizon"], # input: user | type: str | example: "96" | should be int > 0 (default 24)
+        "hyperparms_entrypoint": parameters["hyperameters"], # input: user | type: str | example: "nbeats0_2" | get values from config.yaml headers
         "ignore_previous_runs": parameters["ignore_previous_runs"], # input: user | type: str | example: "true" | allowed values "true" or "false", defaults to false)
     }
 
@@ -141,7 +144,9 @@ async def run_experimentation_pipeline(parameters: dict):
         experiment_name=parameters["experiment_name"]
         )
 
-    return {"parent_run_id": mlflow.tracking.MlflowClient().get_run(pipeline_run.run_id)}
+    # for now send them to MLflow to check their metrics.
+    return {"parent_run_id": mlflow.tracking.MlflowClient().get_run(pipeline_run.run_id),
+            "mlflow_tracking_uri": mlflow.tracking.get_tracking_uri()}
 
 # # find child runs of father run
 # query = "tags.mlflow.parentRunId = '{}'".format(parent_run.info.run_id)
