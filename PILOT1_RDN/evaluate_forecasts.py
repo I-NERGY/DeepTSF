c0ba9b877a74c6faeca6ecf3caed138add7f00c4
theopnt <theopountridis@gmail.com>
2022-09-21 09:22:34 +0300

    Add shap functions

--- PILOT1_RDN/evaluate_forecasts.py
+++ PILOT1_RDN/evaluate_forecasts.py
@@ -69,12 +69,12 @@ def backtester(model,
                past_covariates=None,
                path_to_save_backtest=None):
     """ Does the same job with advanced forecast but much more quickly using the darts
-    bult-in historical_forecasts method. Use this for evaluation. The other only 
-    provides pure inference. Provide a unified timeseries test set point based 
-    on test_start_date. series_transformed does not need to be adjacent to 
+    bult-in historical_forecasts method. Use this for evaluation. The other only
+    provides pure inference. Provide a unified timeseries test set point based
+    on test_start_date. series_transformed does not need to be adjacent to
     training series. if transformer_ts=None then no inverse transform is applied
     to the model predictions.
-    
+
     Parameters
     ----------
 
@@ -130,7 +130,7 @@ def backtester(model,
     # plot one week (better visibility)
     forecast_start_date = pd.Timestamp(
         test_start_date + datetime.timedelta(days=7))
-        
+
     fig2 = plt.figure(figsize=(15, 8))
     ax2 = fig2.add_subplot(111)
     backtest_series \
@@ -149,25 +149,25 @@ def backtester(model,
     test_series = series.drop_before(pd.Timestamp(test_start_date))
     metrics = {
         "mape": mape_darts(
-            test_series, 
+            test_series,
             backtest_series),
         "smape": smape_darts(
-            test_series, 
+            test_series,
             backtest_series),
         "mase": mase_darts(
-            series.drop_before(pd.Timestamp(test_start_date)), 
-            backtest_series, 
+            series.drop_before(pd.Timestamp(test_start_date)),
+            backtest_series,
             insample=series.drop_after(pd.Timestamp(test_start_date))),
         "mae": mae_darts(
-            series.drop_before(pd.Timestamp(test_start_date)), 
+            series.drop_before(pd.Timestamp(test_start_date)),
             backtest_series),
         "rmse": rmse_darts(
-            series.drop_before(pd.Timestamp(test_start_date)), 
+            series.drop_before(pd.Timestamp(test_start_date)),
             backtest_series)
     }
     for key, value in metrics.items():
         print(key, ': ', value)
-    
+
 
     # save plot
     if path_to_save_backtest is not None:
@@ -186,6 +186,115 @@ def backtester(model,
             .to_csv(os.path.join(path_to_save_backtest, 'test_transformed.csv'))
 
     return {"metrics": metrics, "eval_plot": plt, "backtest_series": backtest_series}
+
+def build_shap_dataset(size, train, test, input_chunk_length, output_chunk_length):
+
+    data = []
+    background = []
+    columns = []
+    first_iter = True
+    samples = set()
+    if(type(size) == float):
+        size = int(size*(len(test) - input_chunk_length + 1))
+    if size == len(test) - input_chunk_length + 1:
+        samples = set(range(size))
+    else:
+        for i in range(size):
+            while(True):
+                r = random.randint(0, len(test) - input_chunk_length + 1)
+                if r not in samples:
+                    break
+            samples.add(r)
+
+    for i in samples:
+        curr = test[i:i + input_chunk_length]
+        curr_date = int(curr.time_index[0].timestamp())
+        curr_values = curr.random_component_values(copy=False)
+        if np.isnan(curr_values).any():
+            print(curr_date, "has NaN values")
+            continue
+        data.append(curr_values.flatten())
+#    print(data[-1].flatten())
+        if first_iter:
+            columns.extend([str(i) + " timestep" for i in range(input_chunk_length)])
+            background.extend([train.median(axis=0).values()[0,0] for _ in range(input_chunk_length)])
+        if past_covs != None:
+            for ii in range(past_covs.n_components):
+                data[-1] = np.concatenate([data[-1], past_covs.univariate_component(ii)[i:i + input_chunk_length].random_component_values(copy=False).flatten()])
+                if first_iter:
+                    columns.extend(["Step " + str(iii) + " of past covariate " + str(ii) for iii in range(input_chunk_length)])
+                    background.extend([past_covs.univariate_component(ii).median(axis=0).values()[0,0] for _ in range(input_chunk_length)])
+        if future_covs != None:
+            for ii in range(future_covs.n_components):
+                data[-1] = np.concatenate([data[-1], future_covs.univariate_component(ii)[i:i + input_chunk_length + output_chunk_length].random_component_values(copy=False).flatten()])
+                if first_iter:
+                    columns.extend(["Step " + str(iii) + " of future covariate " + str(ii) for iii in range(input_chunk_length + output_chunk_length)])
+                    background.extend([future_covs.univariate_component(ii).median(axis=0).values()[0,0] for _ in range(input_chunk_length + output_chunk_length)])
+        data[-1] = np.concatenate([data[-1], [curr_date]])
+        if first_iter:
+            columns.extend(["Date"])
+            background.extend([curr_date])
+        first_iter = False
+
+    data = pd.DataFrame(data, columns=columns)
+    background = pd.DataFrame([background], columns=columns)
+    return data, background
+
+def predict(x, n_past_covs, n_future_covs, input_chunk_length, output_chunk_length, model, scaler_list, scale=True):
+    res = []
+    for sample in x:
+        index = [datetime.utcfromtimestamp(sample[-1]) + pd.offsets.DateOffset(hours=i) for i in range(input_chunk_length)]
+        index_future = [datetime.utcfromtimestamp(sample[-1]) + pd.offsets.DateOffset(hours=i) for i in range(input_chunk_length + output_chunk_length)]
+        data = sample[:input_chunk_length]
+        ts = TimeSeries.from_dataframe(pd.DataFrame(data=data, index=index, columns=["Load"]))
+        if scale:
+            ts = scaler_list[0].transform(ts)
+        past_covs = None
+        future_covs = None
+        for i in range(input_chunk_length, input_chunk_length*(n_past_covs+1), input_chunk_length):
+#            print(i, "p")
+            data = sample[i:i+input_chunk_length]
+            if i == input_chunk_length:
+                past_covs = TimeSeries.from_dataframe(pd.DataFrame(data=data, index=index, columns=["Covariate"]))
+                if scale:
+                    past_covs = scaler_list[i//input_chunk_length].transform(past_covs)
+            else:
+                temp = TimeSeries.from_dataframe(pd.DataFrame(data=data, index=index, columns=["Covariate"]))
+                if scale:
+                    past_covs = past_covs.stack(scaler_list[i//input_chunk_length].transform(temp))
+                else:
+                    past_covs = past_covs.stack(temp)
+        for i in range(input_chunk_length*(n_past_covs+1), input_chunk_length*(n_past_covs+1) + (input_chunk_length + output_chunk_length)*n_future_covs, input_chunk_length + output_chunk_length):
+#            print(i, "f")
+            data = sample[i:i+input_chunk_length+output_chunk_length]
+            scale_index = 1 + n_past_covs + (i - input_chunk_length*(n_past_covs+1))//(input_chunk_length+output_chunk_length)
+            if i == input_chunk_length*(n_past_covs+1):
+                future_covs = TimeSeries.from_dataframe(pd.DataFrame(data=data, index=index_future, columns=["Covariate"]))
+                if scale:
+                    future_covs = scaler_list[scale_index].transform(future_covs)
+            else:
+                temp = TimeSeries.from_dataframe(pd.DataFrame(data=data, index=index_future, columns=["Covariate"]))
+                if scale:
+                    future_covs = future_covs.stack(scaler_list[scale_index].transform(temp))
+                else:
+                    future_covs = future_covs.stack(temp)
+
+        ts = model.predict(output_chunk_length, ts, past_covariates=past_covs, future_covariates=future_covs)
+        if scale:
+            res.append(scaler_list[0].inverse_transform(ts).univariate_values())
+        else:
+            res.append(ts.univariate_values())
+   #     print("max", np.array(res).max(), np.array(res).min())
+    #print(np.array(res))
+    return np.array(res)
+#lambda x: model_rnn.predict(TimeSeries.from_dataframe(pd.DataFrame(index=(x[-1] + pd.offsets.DateOffset(hours=i) for i in range(96)), data=x[:-1])))
+
+def call_shap(n_past_covs, n_future_covs, input_chunk_length, output_chunk_length, model, scaler_list, scale):
+    shap.initjs()
+    explainer = shap.KernelExplainer(lambda x : predict(x, n_past_covs, n_future_covs, input_chunk_length, output_chunk_length, model, scaler_list, scale), background)
+    shap_values = explainer.shap_values(data, nsamples="auto", normalize=False)
+    fig = shap.force_plot(explainer.expected_value[0],shap_values[0][0,:], data.iloc[0,:], show=False)
+    mlflow.log_figure(fig, 'force_plot.png')
 
 @click.command()
 @click.option("--mode",
@@ -263,14 +372,14 @@ def evaluate(mode, series_uri, future_covs_uri, past_covs_uri, scaler_uri, cut_d
     series_path = download_online_file(
         series_uri, "series.csv") if mode == 'remote' else series_uri
     series = load_local_csv_as_darts_timeseries(
-        local_path=series_path,  
+        local_path=series_path,
         last_date=test_end_date)
 
     if future_covariates_uri is not None:
         future_covs_path = download_online_file(
             future_covariates_uri, "future_covariates.csv") if mode == 'remote' else future_covariates_uri
         future_covariates = load_local_csv_as_darts_timeseries(
-            local_path=future_covs_path,  
+            local_path=future_covs_path,
             last_date=test_end_date)
     else:
         future_covariates = None
@@ -279,7 +388,7 @@ def evaluate(mode, series_uri, future_covs_uri, past_covs_uri, scaler_uri, cut_d
         past_covs_path = download_online_file(
             past_covariates_uri, "past_covariates.csv") if mode == 'remote' else past_covariates_uri
         past_covariates = load_local_csv_as_darts_timeseries(
-            local_path=past_covs_path,  
+            local_path=past_covs_path,
             last_date=test_end_date)
     else:
         past_covariates = None
@@ -295,20 +404,20 @@ def evaluate(mode, series_uri, future_covs_uri, past_covs_uri, scaler_uri, cut_d
     # Split in the same way as in training
     ## series
     series_split = split_dataset(
-        series, 
-        val_start_date_str=cut_date_test, 
+        series,
+        val_start_date_str=cut_date_test,
         test_start_date_str=cut_date_test,
         test_end_date=test_end_date)
-    
+
     series_transformed_split = split_dataset(
-        series_transformed, 
-        val_start_date_str=cut_date_test, 
+        series_transformed,
+        val_start_date_str=cut_date_test,
         test_start_date_str=cut_date_test,
         test_end_date=test_end_date)
 
     # Evaluate Model
     evaltmpdir = tempfile.mkdtemp()
-    with mlflow.start_run(run_name='eval', nested=True) as mlrun: 
+    with mlflow.start_run(run_name='eval', nested=True) as mlrun:
         mlflow.set_tag("run_id", mlrun.info.run_id)
         mlflow.set_tag("stage", "evaluation")
         evaluation_results = backtester(model=model,
@@ -322,7 +431,21 @@ def evaluate(mode, series_uri, future_covs_uri, past_covs_uri, scaler_uri, cut_d
                                 future_covariates=future_covariates,
                                 past_covariates=past_covariates,
                                 path_to_save_backtest=evaltmpdir)
-                                
+
+        data, background = build_shap_dataset(size=1,####
+                           train=series_split['train'],
+                           test=series_split['val'],
+                           input_chunk_length=96,#####
+                           output_chunk_length=forecast_horizon)
+
+        call_shap(0,####
+                  0, ####
+                  96, ####
+                  input_chunk_length=forecast_horizon,
+                  model=model,
+                  scaler_list=[scaler],
+                  scale=True)####
+
         series_split['test'].to_csv(
             os.path.join(evaltmpdir, "test.csv"))
 
@@ -331,7 +454,7 @@ def evaluate(mode, series_uri, future_covs_uri, past_covs_uri, scaler_uri, cut_d
 
         mlflow.log_metrics(evaluation_results["metrics"])
         mlflow.log_artifacts(evaltmpdir, "eval_results")
-        
+
         print("\nArtifacts uploaded. Deleting local copies...")
         logging.info("\nArtifacts uploaded. Deleting local copies...")
 
